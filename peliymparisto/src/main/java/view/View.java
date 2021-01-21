a01dcf7a12d973c19328b3365c9d5e6f2b67f7cf
akikop <aki.koppinen@metropolia.fi>
2021-01-21 14:43:16 +0200

    Added Starting code to UI

--- peliymparisto/src/main/java/view/View.java
+++ peliymparisto/src/main/java/view/View.java
@@ -1,10 +1,77 @@
 package view;
 
-public class View implements ViewIF {
+import java.text.DecimalFormat;
+
+import controller.*;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class View extends Application implements ViewIF {
+	
+	//variables here -->
+	private ControllerIF controller;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
+	
+	//setting starting values
+	@Override
+	public void init(){
+		controller = new Controller(); //add parameter later
+	}	
 
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			
+			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			    @Override
+			    public void handle(WindowEvent t) {
+			        Platform.exit();
+			        System.exit(0);
+			    }
+			});
+			
+			primaryStage.setTitle("GameEnvironment");
+			
+			BorderPane mainMenu = mainMenuBuilder();
+			BorderPane pokerGame = pokerGameBuilder();
+			BorderPane settings = settingsBuilder();
+			
+	        Scene mainMenuScene = new Scene(mainMenu);
+	        Scene pokerGameScene = new Scene(pokerGame);
+	        Scene settingsScene = new Scene(settings);
+	        
+	        //createGUITransition() continue making transitions with this function
+	        
+	        primaryStage.setScene(mainMenuScene);
+	        primaryStage.show();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private BorderPane settingsBuilder() {
+		BorderPane something = new BorderPane();
+		return something;
+	}
+
+	private BorderPane pokerGameBuilder() {
+		BorderPane something = new BorderPane();
+		return something;
+	}
+
+	private BorderPane mainMenuBuilder() {
+		BorderPane something = new BorderPane();
+		return something;
 	}
 
 }
