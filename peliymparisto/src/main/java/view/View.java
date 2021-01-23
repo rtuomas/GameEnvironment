250eabd6446fd90c1f98062f1466250605ec012a
akikop <aki.koppinen@metropolia.fi>
2021-01-23 12:05:32 +0200

    Added transition buttons between mainmenu, pokergame and settings. Commented the existing code.

--- peliymparisto/src/main/java/view/View.java
+++ peliymparisto/src/main/java/view/View.java
@@ -5,14 +5,29 @@ import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 public class View extends Application implements ViewIF {
 	
-	//variables here -->
+	//Normal variables here -->
+	/** Controller which transfers info per mvc structure */
 	private ControllerIF controller;
+	
+	//GUI components here -->
+	/** Button to enter the poker game view */
+	private Button enterPokerGame;
+	/** Button to enter the settings view */
+	private Button enterSettings;
+	/** Button to return to the mainMenu view from poker game*/
+	private Button backToMainMenu1;
+	/** Button to return to the mainMenu view from settings */
+	private Button backToMainMenu2;
+	/** Button to close the program */
+	private Button exitProgram;
 
 	public static void main(String[] args) {
 		launch(args);
@@ -28,6 +43,7 @@ public class View extends Application implements ViewIF {
 	public void start(Stage primaryStage) {
 		try {
 			
+			// this will handle the safe closing of the program
 			primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
 			    @Override
 			    public void handle(WindowEvent t) {
@@ -46,7 +62,7 @@ public class View extends Application implements ViewIF {
 	        Scene pokerGameScene = new Scene(pokerGame);
 	        Scene settingsScene = new Scene(settings);
 	        
-	        //createGUITransition() continue making transitions with this function
+	        createGUITransitions(primaryStage, mainMenuScene, pokerGameScene, settingsScene);
 	        
 	        primaryStage.setScene(mainMenuScene);
 	        primaryStage.show();
@@ -54,22 +70,69 @@ public class View extends Application implements ViewIF {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
 	}
 	
-	private BorderPane settingsBuilder() {
-		BorderPane something = new BorderPane();
-		return something;
-	}
-
-	private BorderPane pokerGameBuilder() {
-		BorderPane something = new BorderPane();
-		return something;
-	}
-
+	/**
+	 * Contains the GUI for main menu
+	 * @return BorderPane type layout for the main menu
+	 */
 	private BorderPane mainMenuBuilder() {
-		BorderPane something = new BorderPane();
-		return something;
+		BorderPane mainMenuView = new BorderPane();
+		mainMenuView.setPrefSize(400, 400);
+		enterPokerGame = new Button("Pokeri");
+		enterSettings = new Button("Asetukset");
+		exitProgram = new Button("Lopeta");
+		VBox napit = new VBox();
+		napit.getChildren().addAll(enterPokerGame, enterSettings, exitProgram);
+		mainMenuView.setCenter(napit);
+		return mainMenuView;
+	}
+	
+	/**
+	 * Contains the GUI for poker game
+	 * @return BorderPane type layout for the poker game
+	 */
+	private BorderPane pokerGameBuilder() {
+		BorderPane pokerGameView = new BorderPane();
+		pokerGameView.setPrefSize(400, 400);
+		backToMainMenu1 = new Button("Takaisin");
+		pokerGameView.setTop(backToMainMenu1);
+		return pokerGameView;
+	}
+	
+	/**
+	 * Contains the GUI for settings
+	 * @return BorderPane type layout for the settings
+	 */
+	private BorderPane settingsBuilder() {
+		BorderPane settingsView = new BorderPane();
+		settingsView.setPrefSize(400, 400);
+		backToMainMenu2 = new Button("Takaisin");
+		settingsView.setTop(backToMainMenu2);
+		return settingsView;
+	}
+
+	/**
+	 * This function will create the transitions between different views and also the exit button functionality
+	 * @param primaryStage Current View in the program
+	 * @param mainMenuScene Main menu view
+	 * @param pokerGameScene Poker game view
+	 * @param settingsScene Settings view
+	 */
+	private void createGUITransitions(Stage primaryStage, Scene mainMenuScene, Scene pokerGameScene, Scene settingsScene) {
+		enterPokerGame.setOnAction(e -> {
+			primaryStage.setScene(pokerGameScene);
+		});
+		enterSettings.setOnAction(e -> {
+			primaryStage.setScene(settingsScene);
+		});
+		backToMainMenu1.setOnAction(e -> {
+			primaryStage.setScene(mainMenuScene);
+		});
+		backToMainMenu2.setOnAction(e -> {
+			primaryStage.setScene(mainMenuScene);
+		});
+		exitProgram.setOnAction(e -> Platform.exit());
 	}
 
 }
