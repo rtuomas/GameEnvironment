294783bd6990b885be152d9931f68c0d4f0c4481
Tatu Nordström <Tatu.Nordstrom@metropolia.fi>
2021-01-27 15:08:11 +0200

    Card class.

--- peliymparisto/src/main/java/model/Card.java
+++ peliymparisto/src/main/java/model/Card.java
@@ -1,5 +1,28 @@
 package model;
 
-public class Card {
 
+public class Card {
+	
+    final static String[] SUITS = {"Heart", "Diamond", "Club", "Spade"};
+    final static int HEART = 0, DIAMOND = 1, CLUB = 2, SPADE = 3;
+    
+     private int value;            			
+     private int suit;
+
+    public Card (int suit, int value) {
+    	this.suit = suit;
+    	this.value = value;
+    }
+    
+    public int getMaa () {
+    	return suit;
+    }
+    
+    public int getArvo () {
+    	return value;
+    }
+
+    public String toString () {
+    	return (SUITS[suit] + " " + value);
+    }
 }
