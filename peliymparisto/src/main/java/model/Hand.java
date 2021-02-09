f87f46bf3cbdfc4141491af59bd28cba73ca6d67
Ville Riepponen <villeriepponen@gmail.com>
2021-02-09 13:01:46 +0200

    Hand class tweak, adding win conditions

--- peliymparisto/src/main/java/model/Hand.java
+++ peliymparisto/src/main/java/model/Hand.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -9,14 +11,17 @@ import java.util.List;
  * @version 0.1 28.01.2021
  */
 
-public class Hand {
+public class Hand{
 	final static int HANDSIZE = 5;
 	private Card[] hand = new Card [HANDSIZE];
+	private Card[] sortedHand = new Card [HANDSIZE];
 	private Deck currentDeck;
 	
 	public Hand (Deck deck){
 		this.currentDeck = deck;
 		fillHandWithCards();
+		this.sortedHand = this.hand;
+		sortSortedHand();
 	}
 	
 	private void fillHandWithCards(){
@@ -25,8 +30,25 @@ public class Hand {
 		}
 	}
 	
+	
+	
+	private void sortSortedHand() {
+		Arrays.sort(sortedHand, new Comparator<Card>() {
+			@Override
+			public int compare(Card c1, Card c2) {
+				return c1.getValue() - c2.getValue();
+			}
+		});
+	}
+	
+	
+	
 	public Card[] getHand() {
 		return this.hand;
+	}
+	
+	public Card[] getSortedHand() {
+		return this.sortedHand;
 	}
 	
 	//The player can choose which cards they want to swap to new ones once per round.
@@ -34,11 +56,12 @@ public class Hand {
 	//swapCards() then adds the remaining cards from the deck to fill the hand again,
 	//returning the old hand with the new cards.
 	
-	public Card[] swapCards(Card[] sentHand) {
-		for (int i = sentHand.length; i < HANDSIZE; i++) {
-			hand[i] = currentDeck.nextCard();
+	public void swapCards(Card[] sentHand) {
+		for (int i = 0; i < HANDSIZE; i++) {
+			if(hand[i] == null) {
+				hand[i] = currentDeck.nextCard();
+			}
 		}
-		return sentHand;
 	}
 	
 	public String toString() {
@@ -52,5 +75,58 @@ public class Hand {
 			
 		}
 		return tempString;
+	}
+	
+	public boolean isSet() {
+		boolean isSet = false;
+		int c1 = sortedHand[0].getValue(), 
+				c2 = sortedHand[1].getValue(), 
+				c3 = sortedHand[2].getValue(), 
+				c4 = sortedHand[3].getValue(), 
+				c5 = sortedHand[4].getValue();
+		if(c1 == c2 && c2 == c3 && c4 != c1 && c5 != c1 && c3 != c4) {
+			isSet = true;
+		} else if(c2 == c3 && c3 == c4 && c1 != c2 && c5 != c2 && c1 != c5) {
+			isSet = true;
+		} else if(c3 == c4 && c4 == c5 && c1 != c3 && c2 != c3 && c1 != c2) {
+			isSet = true;
+		} else {
+			isSet = false;
+		}
+		
+		return isSet;
+	}
+	
+	public boolean isFullHouse() {
+		boolean isFullHouse = false;
+		int c1 = sortedHand[0].getValue(), 
+				c2 = sortedHand[1].getValue(), 
+				c3 = sortedHand[2].getValue(), 
+				c4 = sortedHand[3].getValue(), 
+				c5 = sortedHand[4].getValue();
+		if(c1 == c2 && c2 == c3 && c4 == c5) {
+			isFullHouse = true;
+		} else if(c1 == c2 && c3 == c4 && c4 == c5) {
+			isFullHouse = true;
+		} else {
+			isFullHouse = false;
+		}
+		
+		return isFullHouse;
+	}
+	
+	public boolean isTwoPairs() {
+		boolean isTwoPairs = false;
+		if(hand[0].getValue() == hand[1].getValue() && hand[2].getValue() == hand[3].getValue()) {
+			isTwoPairs = true;
+		} else if(hand[0].getValue() == hand[1].getValue() && hand[3].getValue() == hand[4].getValue()) {
+			isTwoPairs = true;
+		} else if(hand[1].getValue() == hand[2].getValue() && hand[3].getValue() == hand[4].getValue()) {
+			isTwoPairs = true;
+		} else {
+			isTwoPairs = false;
+		}
+		
+		return isTwoPairs;
 	}
 }
---
+++ peliymparisto/src/test/java/tests/HandTest.java
@@ -0,0 +1,105 @@
+package tests;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import model.Card;
+import model.Deck;
+import model.Hand;
+
+class HandTest {
+	
+	Deck deck = new Deck();
+	Hand hand = new Hand(deck);
+	Card[] cards = new Card[5];
+	Card card;
+	
+	/*
+	@BeforeEach
+	void setUp() {
+		deck.shuffle();
+	}
+	*/
+
+	@Test
+	void testHand() {
+		System.out.println("Hand tested");
+		assertEquals("1H, 2H, 3H, 4H, 5H", hand.toString());
+	}
+	
+	@Test
+	void testSortedHand() {
+		deck.shuffle();
+		hand = new Hand(deck);
+		Card[] sortedHand = hand.getSortedHand();
+		boolean sorted = false;
+		for(int i = 0; i<4; i++) {
+			if(sortedHand[i].getValue() > sortedHand[i+1].getValue()) {
+				sorted = false;
+				break;
+			}
+			if(i == 3) {
+				sorted = true;
+			}
+		}
+		System.out.println("Sorted hand tested");
+		System.out.println(hand.toString());
+		assertTrue("Hand is not sorted", sorted);
+	}
+	
+	@Test
+	void testSwapCards() {
+		Card[] handToSwap = hand.getHand();
+		handToSwap[0] = null;
+		handToSwap[3] = null;
+		hand.swapCards(handToSwap);
+		System.out.println("Swap cards tested");
+		System.out.println(hand.toString());
+		assertEquals("6H, 2H, 3H, 7H, 5H", hand.getHand());
+		//SwapCards works, but implementing this to work with view might be a problem
+		
+	}
+	
+	@Test
+	void testSet() {
+		int rounds = 0;
+		do {
+			deck = new Deck();
+			deck.shuffle();
+			hand = new Hand(deck);
+			rounds++;
+		} while(!hand.isSet());
+		System.out.println(hand.toString() + " " + rounds + " rounds, set");
+		assertTrue(hand.isSet());
+	}
+	@Test
+	void testTwoPairs() {
+		int rounds = 0;
+		do {
+			deck = new Deck();
+			deck.shuffle();
+			hand = new Hand(deck);
+			rounds++;
+		} while(!hand.isTwoPairs());
+		System.out.println(hand.toString() + " " + rounds + " rounds, two pairs");
+		assertTrue(hand.isTwoPairs());
+	}
+	
+	@Test
+	void testFullHouse() {
+		int rounds = 0;
+		do {
+			deck = new Deck();
+			deck.shuffle();
+			hand = new Hand(deck);
+			rounds++;
+		} while(!hand.isFullHouse());
+		System.out.println(hand.toString() + " " + rounds + " rounds, fullhouse");
+		assertTrue(hand.isFullHouse());
+	}
+
+}
