0261534accc57e1e3729f926993ff66547e751d2
Ville Riepponen <villeriepponen@gmail.com>
2021-02-09 13:29:39 +0200

    Hand class tweak, adding win conditions

---
+++ peliymparisto/src/test/java/model/HandTest.java
@@ -0,0 +1,14 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HandTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
