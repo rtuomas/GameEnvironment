f6bfb0e1528b77365e3e4a7e2fcace8c250589d7
jerelam <jere.lampola@live.fi>
2021-05-07 11:39:39 +0300

    Javadoc for deck

--- peliymparisto/src/main/java/model/Deck.java
+++ peliymparisto/src/main/java/model/Deck.java
@@ -1,5 +1,8 @@
 /** =============================== Javadoc ====================================
+* Class contains a number of card objects that can be used in games. Has basic
+* methods needed for shuffling
 * @author Jere Lampola
+* @version 1.1 2021-05-07
 */
 
 package model;
@@ -7,21 +10,30 @@ package model;
 public class Deck {
   /* ============================= Variables ================================ */
   private int    size         = 52;
-  private int    jokers       = 0;
   private Card[] deck         = new Card[size];
   private int    nextIndex    = 0;
   private int    shuffleSwaps = 10000;
   
   /* ============================ Constructors ============================== */
+  /**
+   * Constructor, the deck is filled with cards during creation.
+   */
   public Deck(){
     fillDeckWithCards();
   }
   
   /* =========================== Public methods ============================= */
+  /**
+   * Method gives the card object at the next index
+   * @return next card in the deck
+   */
   public Card nextCard(){
     return deck[getNextIndex()];
   }
   
+  /**
+   * Method for shuffling the card array.
+   */
   public void shuffle(){
     for(int i=0;i<shuffleSwaps;i++){
       swapTwoCards();
@@ -29,16 +41,27 @@ public class Deck {
   }
   
   /* ========================== Private methods ============================= */
+  /**
+   * Fills the deck, number of cards depends on the size variable
+   */
   private void fillDeckWithCards(){
     for(int i=0;i<size;i++){
       deck[i] = new Card(i/13%4,i%13+1);
     }
   }
 
+  /**
+   * Increases the current index and returns it. Index will be limited to size
+   * with modulo operator.
+   * @return
+   */
   private int getNextIndex(){
     return nextIndex++%size; // Overflow on index (2^32 / 2 + 1)...
   }
 
+  /**
+   * Takes two cards randomly in the deck and changes their places head to head.
+   */
   private void swapTwoCards(){
     int[]  indexes   = getTwoUniqueRandomNumbers(0, size - 1);
     Card temporary   = deck[indexes[0]];
@@ -46,6 +69,12 @@ public class Deck {
     deck[indexes[1]] = temporary;
   }
   
+  /**
+   * A method for generating two unique random numbers
+   * @param min : Lower bound for random numbers
+   * @param max : Upper bound for random numbers
+   * @return integer array containing the two numbers
+   */
   private int[] getTwoUniqueRandomNumbers(int min, int max){
     int firstRandom  = getRandomNumber(min, max);
     int secondRandom = firstRandom;
@@ -56,6 +85,12 @@ public class Deck {
     return twoRandoms;
   }
   
+  /**
+   * Generates a random within the limits
+   * @param min : lower limit
+   * @param max : upper limit
+   * @return the random integer
+   */
   private int getRandomNumber(int min, int max){
     return (int)(Math.random()*(max-min+1)+min);
   }
